e4cc3dc8a63e0a4e39e65075aa244f0fe26dea44
张少林-kde <zsl@kuba.bz>
2016-04-22 16:18:06 +0800

    change: update file headers

--- mysql-bundle.js
+++ mysql-bundle.js
@@ -1,3 +1,9 @@
+/*!
+ * mysql-bundle - mysql-bundle.js
+ * Copyright(c) 2016 LnsooXD <LnsooXD@gmail.com>
+ * MIT Licensed
+ */
+
 exports = module.exports = {};
 
 var Client = require('./lib/client');
