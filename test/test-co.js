837fa0cf6052bf42b9eaeed384b8cf03d62a9c7f
LnsooXD <LnsooXD@gmail.com>
2016-04-28 17:46:13 +0800

    change: replace util.each with ctrl-it module

--- test/test-co.js
+++ test/test-co.js
@@ -1,19 +1,13 @@
 "use strict";
-const CTRL_NORMAL = 100;
-const CTRL_CONTINUE = 101;
-const CTRL_BREAK = 102;
-
 const mysql = require('../');
-const util = require('../lib/util');
+const it = require('ctrl-it');
 const co = require('co');
 
 const alpha0 = mysql.obtainCo('alpha0');
 const alpha1 = mysql.obtainCo('alpha1');
 const beta0 = mysql.obtainCo('beta0');
 const beta1 = mysql.obtainCo('beta1');
-
 const clients = [alpha0, alpha1, beta0, beta1];
-
 
 const userDataMap = {
 	username: 'TDSam',
@@ -36,7 +30,7 @@ const updateNicknameData = {
 };
 
 co(function*() {
-	yield every(clients, function*(i, client) {
+	yield it.every(clients, function*(i, client) {
 		yield dropAndCreate(client);
 	});
 
@@ -62,15 +56,4 @@ function *dropAndCreate(client) {
 	console.log('drop result: ', rows);
 	rows = yield client.queryTemplate('create_user_table');
 	console.log('create result: ', rows);
-}
-
-function *every(obj, eachCb) {
-	for (let k in obj) {
-		if (obj.hasOwnProperty(k)) {
-			let res = yield eachCb(k, obj[k]);
-			if (res === CTRL_BREAK) {
-				break;
-			}
-		}
-	}
 }
